4622ee4492a9b61ce5012bf9fd47dcae0124bd0d
Pallavi Soam <pallavisoam35@gmail.com>
2022-11-13 20:30:57 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+var cors = require('cors')
+const sendSms = require('./twilio')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+app.use(bodyParser.json())
+
+const port = 5000
+
+// Create users endpoint
+app.post('/users', (req, res) => {
+    const { text, phone } = req.body
+    const user = {
+        text,
+        phone
+    }
+    sendSms(user.phone, user.text)
+
+    res.status(201).send({
+        message: 'Account created successfully, kindly check your phone to activate your account!',
+        data: user
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
+
+module.exports = app
